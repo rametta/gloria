e94475a57d3fa442e27bb40866ba8b103a220b85
Jason <jrametta@dynamite.ca>
2019-05-16 11:31:17 -0400

    now / travis config

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,4 @@ export = (app: Application) => {
     const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
     await context.github.issues.createComment(issueComment)
   })
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
 }
