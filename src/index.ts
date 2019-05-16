8e7814b398fa3451d7fe8bde80e1a7479424ad8b
Jason <jrametta@dynamite.ca>
2019-05-16 14:56:59 -0400

    better export strategy

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,10 @@ import { Application } from 'probot'
 import { serverless } from '@chadfawcett/probot-serverless-now'
 
 const bot = (app: Application) => {
-  app.on('issues.opened', async (context) => {
+  app.on('issues.edited', async (context) => {
     const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
     await context.github.issues.createComment(issueComment)
   })
 }
 
-export = process.env.NODE_ENV === 'production' ? serverless(bot) : bot
+export = process.env.NODE_ENV === 'dev' ? bot : serverless(bot)
