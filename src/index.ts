6dfd24f4ab90d41cb193f5e7fc0215759a5fd01a
Jason <jrametta@dynamite.ca>
2019-05-16 11:59:09 -0400

    use serverless package

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import { Application } from 'probot'
+import { serverless } from '@chadfawcett/probot-serverless-now'
 
-export = (app: Application) => {
+const bot = (app: Application) => {
   app.on('issues.opened', async (context) => {
     const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
     await context.github.issues.createComment(issueComment)
   })
 }
+
+export = process.env.NODE_ENV === 'production' ? serverless(bot) : bot
