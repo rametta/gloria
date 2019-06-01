c3a21ef300ec4c37f0e9ec325b39fb4a2d373a0f
Jason <jrametta@dynamite.ca>
2019-06-01 10:07:16 -0400

    tickets

--- src/config.ts
+++ src/config.ts
@@ -4,14 +4,14 @@ export interface Config {
   SET_TICKETS: boolean
   POST_CHECKS: boolean
   JIRA_LINK: string
-  TICKET_PREFIX: string
+  TICKET_PREFIXS: string[]
 }
 
 const defaultConfig: Config = {
   SET_TICKETS: false,
   POST_CHECKS: false,
   JIRA_LINK: '',
-  TICKET_PREFIX: ''
+  TICKET_PREFIXS: []
 }
 
 export const getConfig = (context: Context): Promise<Config> =>
--- src/prTickets.ts
+++ src/prTickets.ts
@@ -14,41 +14,66 @@ const generateLink = (base: string) => (ticket: string) =>
     ? base + ticket
     : base + '/' + ticket
 
-const extractTickets = (jira: string) => (prefix: string) => (title?: string) =>
+const extractTickets = (jira: string) => (prefixs: string[]) => (title?: string) =>
   Maybe(title)
-    .chain(t => Maybe(t.match(new RegExp(prefix + '-\\d+', 'gi'))))
-    .map(tickets => tickets.map(t => tableAnchor(t)(generateLink(jira)(t))))
+    .map(t => prefixs.map(prefix => Maybe(t.match(new RegExp(prefix + '-\\d+', 'gi')))))
+    .map(matches => matches.filter(m => m.isJust()))
+    .chain(matches => matches.length ? Just(matches) : Nothing)
+    .map((matches: RegExpMatchArray[]) => matches.reduce((acc, tickets) => acc.concat(tickets.map(t => tableAnchor(t)(generateLink(jira)(t)))), []))
 
 const isBot = (ctx: Context): Maybe<Context> => ctx.isBot
   ? Nothing
   : Just(ctx)
 
 const getTickets = (config: Config) => (ctx: Context): Maybe<string[]> =>
-  (config.SET_TICKETS && config.JIRA_LINK !== '' && config.TICKET_PREFIX !== '')
-    ? getTitle(ctx).chain(title => extractTickets(config.JIRA_LINK)(config.TICKET_PREFIX)(title))
+  (config.SET_TICKETS && config.JIRA_LINK !== '' && config.TICKET_PREFIXS.length)
+    ? getTitle(ctx).chain(title => extractTickets(config.JIRA_LINK)(config.TICKET_PREFIXS)(title))
     : Nothing
 
 const joinTickets = (tickets: string[]) =>
   `<!--ticketstart-->
-  #### 📃 Tickets
+  #### :page_facing_up: Tickets
   <table><tr>
   ${tickets.join('')}
   </tr></table>
   <!--ticketend-->\n`
 
-const shouldAddTickets = (ctx: Context) => (tickets: string[]): Maybe<string> =>
+const shouldModifyTickets = (body: string) => (tickets: string[]): Maybe<string> =>
+  Just(body.slice(body.indexOf('<!--ticketstart-->'), body.indexOf('<!--ticketend-->') + 16))
+    .map(oldMiddle => ({
+      oldMiddle,
+      newMiddle: joinTickets(tickets)
+    }))
+    .chain(({ oldMiddle, newMiddle }) =>
+      oldMiddle === newMiddle
+        ? Nothing
+        : modifyBodyTickets(body)(newMiddle)
+    )
+
+const modifyBodyTickets = (body: string) => (middle: string): Maybe<string> =>
+  Just({
+    before: body.slice(0, body.indexOf('<!--ticketstart-->')),
+    after: body.slice(body.indexOf('<!--ticketend-->') + 16)
+  })
+  .map(({ before, after }) => before + middle + after)
+
+const addTicketsToBody = (body: string) => (tickets: string[]): Maybe<string> =>
+  Just(joinTickets(tickets) + body)
+
+const shouldModifyBody = (ctx: Context) => (tickets: string[]): Maybe<string> =>
   getBody(ctx)
     .chain(body => body.indexOf('<!--ticketstart-->') === -1
-      ? Just(joinTickets(tickets) + body)
-      : Nothing
+      ? addTicketsToBody(body)(tickets)
+      : shouldModifyTickets(body)(tickets)
     )
 
 export const prTickets = (ctx: Context): Promise<void> => 
   getConfig(ctx)
     .then(config => {
+      console.log(config)
       isBot(ctx)
         .chain(getTickets(config))
-        .chain(shouldAddTickets(ctx))
+        .chain(shouldModifyBody(ctx))
         .cata({
           Just: body => ctx.github.pullRequests.update(ctx.issue({ body })),
           Nothing: () => ctx.log('PR Body not updated with tickets')
